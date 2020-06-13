fd1910f87e0616d07d6bd32fd4375ee505a40494
dazcha76 <dazcha@hotmail.com>
2020-06-13 14:11:04 -0700

    solved exercise 1

--- exercise01.js
+++ exercise01.js
@@ -8,3 +8,35 @@
 
 // Input: 'I Love Code'
 // Output: edoC evoL I
+
+// front to back
+
+let frontFirstReverse = str => {
+    let reversed = '';
+    for(let i = 0; i < str.length; i++){
+        reversed = str[i] + reversed;
+    }
+    return reversed;
+}
+
+console.log(frontFirstReverse('coderbyte'));
+
+// back to front
+
+let backFirstReverse = str => {
+    let reversed = '';
+    for(let i = str.length - 1; i >= 0; i--){
+        reversed += str[i];
+    }
+    return reversed;
+}
+
+console.log(backFirstReverse('I Love Code'));
+
+// with method
+
+let methodFirstReverse = str => {
+    return str.split('').reverse().join('');
+}
+
+console.log(methodFirstReverse('First Reverse'))
